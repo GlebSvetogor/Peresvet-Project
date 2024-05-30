c378280a69971a8f2e0ff2ffe062f94adfc355c9
Gleb Svetogor <asabsiu@mail.ru>
2024-05-30 18:41:38 +0300

    Add event handler for links in navigation (navigation.js)

--- Peresvet-Project/wwwroot/js/navigation.js
+++ Peresvet-Project/wwwroot/js/navigation.js
@@ -1,23 +1,18 @@
-﻿const liElements = document.querySelectorAll(".nav__list-item");
-const activeLiClassName = "nav__list-item--active";
-const indexTextValue = "Главная";
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const liElements = document.querySelectorAll(".nav__list-item");
+    const activeLiClassName = "nav__list-item--active"
 
-liElements.forEach((li) =>
-    li.addEventListener('click', () => {
-        if (!li.classList.contains(activeLiClassName)) {
-            console.log(li);
-            const activeLiElement = Array.from(liElements).find(el => el.classList.contains(activeLiClassName));
-            if (activeElement != null) {
-                activeLi.classList.remove(activeLiClassName);
-                li.classList.add(activeLiClassName);
-            }else {
-                const liWithMainLink = Array.from(liElements).find(li => {
-                    const link = li.querySelector('a.nav__list-link');
-                    return link && link.textContent.trim() === searchText;
-                });
-                liWithMainLink.classList.add(activeLiClassName);
-            }
-        }
-
+    liElements.forEach((li) => {
+        li.addEventListener('click', () => {
+            liElements.forEach(li => li.classList.remove(activeLiClassName));
+            li.classList.add(activeLiClassName);
+        });
     })
-);
+    const currentPath = window.location.pathname;
+    liElements.forEach((li) => {
+        const link = li.querySelector('a.nav__list-link');
+        if (link && link.getAttribute('href') == currentPath) {
+            li.classList.add(activeLiClassName);
+        }
+    })
+})
